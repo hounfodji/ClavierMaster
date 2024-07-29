68bd77d29d902cd4c2d4f9cd055fa53d97de33aa
hounfodji <hospicehounfodji@gmail.com>
2024-07-29 16:15:15 +0100

    Utilistation des fonctions

---
+++ config.js
@@ -0,0 +1,7 @@
+// liste des mots utilisés pour le jeu
+let listeMots = ["Cachalot", "Pétunia", "Serviette"];
+let listePhrases = [
+  "Pas de panique !",
+  "La vie, l'univers et le reste",
+  "Merci pour le poisson",
+];
--- script.js
+++ script.js
@@ -1,41 +1,55 @@
-let listeMots = ["Cachalot", "Pétunia", "Serviette"];
-let listePhrases = [
-  "Pas de panique !",
-  "La vie, l'univers et le reste",
-  "Merci pour le poisson",
-];
-let score = 0;
-let motUtilisateur = "";
+// let score = 0;
+// let motUtilisateur = "";
+let scoreFinal = 0;
+// cette fonction prend en paramètre le résultat et le nombre total de mots proposés, et affiche le résultat du joueur ;
+function afficherResultat(score, nombreMots) {
+  let message = "Votre score est de " + score + " sur " + nombreMots;
+  return message;
+}
 
-let choix = prompt(
-  "Voulez vous une liste de mots ou de phrases? Entrez 'mots' ou 'phrases'."
-);
-
-while (choix !== "mots" && choix !== "phrases") {
-  choix = prompt(
+// cette fonction demande à l’utilisateur s’il veut jouer avec des phrases ou des mots.
+function choisirPhrasesOuMots() {
+  let choix = prompt(
     "Voulez vous une liste de mots ou de phrases? Entrez 'mots' ou 'phrases'."
   );
+  while (choix !== "mots" && choix !== "phrases") {
+    choix = prompt(
+      "Voulez vous une liste de mots ou de phrases? Entrez 'mots' ou 'phrases'."
+    );
+  }
+  return choix;
 }
 
-switch (choix) {
-  case "mots":
-    for (let index = 0; index < listeMots.length; index++) {
-      motUtilisateur = prompt("Tapez le mot: " + listeMots[index]);
-      if (motUtilisateur === listeMots[index]) {
-        score += 1;
-      }
+// cette fonction contient la boucle principale de jeu, c'est-à-dire la boucle for qui propose les mots/phrases au joueur, et lui demande de taper ces mots. Elle prend en paramètre le tableau de mots/phrases qui sera proposé au joueur, et retourne le nombre de mots/phrases correctement tapés ;
+function lancerBoucleDeJeu(table) {
+  let score = 0;
+  for (let index = 0; index < table.length; index++) {
+    let motUtilisateur = prompt("Tapez: " + table[index]);
+    if (motUtilisateur === table[index]) {
+      score += 1;
     }
-    break;
-  case "phrases":
-    for (let index = 0; index < listePhrases.length; index++) {
-      motUtilisateur = prompt("Tapez le mot: " + listePhrases[index]);
-      if (motUtilisateur === listePhrases[index]) {
-        score += 1;
-      }
-    }
-    break;
-  default:
-    break;
+  }
+  return score;
 }
 
-console.log(score);
+// cette fonction sera la fonction principale, c’est elle qui s’occupe de lancer toutes les autres. En d’autres termes, c’est elle qui va appeler les fonctions que vous venez d’écrire.
+function lancerJeu() {
+  let choix = choisirPhrasesOuMots();
+  let n = 0;
+  switch (choix) {
+    case "mots":
+      scoreFinal = lancerBoucleDeJeu(listeMots);
+      n = listeMots.length;
+      break;
+    case "phrases":
+      scoreFinal = lancerBoucleDeJeu(listePhrases);
+      n = listePhrases.length;
+      break;
+    default:
+      break;
+  }
+
+  console.log(afficherResultat(scoreFinal, n));
+}
+
+lancerJeu();
