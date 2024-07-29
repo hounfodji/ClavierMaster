d5970b48adf13569d566171a77859996c89ba9d4
hounfodji <hospicehounfodji@gmail.com>
2024-07-29 15:44:54 +0100

    Utilisation des boucles

--- script.js
+++ script.js
@@ -1,21 +1,41 @@
-let listeMots = ["Cachalot", "Pétunia", "Serviette"]
-let score = 0
+let listeMots = ["Cachalot", "Pétunia", "Serviette"];
+let listePhrases = [
+  "Pas de panique !",
+  "La vie, l'univers et le reste",
+  "Merci pour le poisson",
+];
+let score = 0;
+let motUtilisateur = "";
 
-let motUtilisateur = prompt("Tapez le mot: " + listeMots[0])
+let choix = prompt(
+  "Voulez vous une liste de mots ou de phrases? Entrez 'mots' ou 'phrases'."
+);
 
-if (motUtilisateur === listeMots[0]) {
-    score += 1
+while (choix !== "mots" && choix !== "phrases") {
+  choix = prompt(
+    "Voulez vous une liste de mots ou de phrases? Entrez 'mots' ou 'phrases'."
+  );
 }
 
-motUtilisateur = prompt("Tapez le mot: " + listeMots[1])
-
-if (motUtilisateur === listeMots[1]) {
-    score += 1
+switch (choix) {
+  case "mots":
+    for (let index = 0; index < listeMots.length; index++) {
+      motUtilisateur = prompt("Tapez le mot: " + listeMots[index]);
+      if (motUtilisateur === listeMots[index]) {
+        score += 1;
+      }
+    }
+    break;
+  case "phrases":
+    for (let index = 0; index < listePhrases.length; index++) {
+      motUtilisateur = prompt("Tapez le mot: " + listePhrases[index]);
+      if (motUtilisateur === listePhrases[index]) {
+        score += 1;
+      }
+    }
+    break;
+  default:
+    break;
 }
 
-motUtilisateur = prompt("Tapez le mot: " + listeMots[2])
-
-if (motUtilisateur === listeMots[2]) {
-    score += 1
-}
 console.log(score);
